46239e6c130420aa1a66d8bc50a6504c77101b35
Deepak R N <deepak160400@gmail.com>
2026-07-29 14:41:09 +0530

    Delete app/routes/home.py

--- app/routes/home.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask import render_template
-
-home_bp = Blueprint("home", __name__)
-
-
-@home_bp.route("/")
-def home():
-    return render_template("index.html")
